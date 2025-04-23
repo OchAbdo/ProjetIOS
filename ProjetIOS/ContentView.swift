f8c6d921b5c9c0fde21873cc5cc4d66a0fabc7ee
Tekup-mac-4 <tekup-mac-4@Tekup-mac-4s-Mac-mini.local>
2025-04-23 14:27:41 +0100

    add in Acceuil

--- ProjetIOS/ContentView.swift
+++ ProjetIOS/ContentView.swift
@@ -11,34 +11,67 @@ struct ContentView: View {
     var body: some View {
         VStack {
             VStack {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                    .padding(.bottom , 300)
+                    .padding(.leading , -35)
             }
             .frame(
-                width: 500,
-                height: 500
-            )
+                maxWidth: .infinity,
+                maxHeight: 500)
             .background(Color(red: 112/255, green: 211/255, blue: 166/255))
             Spacer()
             
-            VStack{
+            
+            HStack{
+                Image("fleche")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                
                 Text("Explore now")
                     .multilineTextAlignment(.center)
                     .foregroundColor(.white)
                     .font(.system(size: 20, weight: .bold, design:.default))
+                    .padding()
                     
             }.frame(
-                width: 390,
-                height: 80
+                maxWidth : .infinity,
+                maxHeight : 80
             )
-            .background(Color(red: 112/255, green: 211/255, blue: 166/255))
-            .cornerRadius(200)
+            .background(
+                RoundedRectangle(cornerRadius: 0 , style: .continuous)
+                    .fill(Color(red: 112/255, green: 211/255, blue: 166/255))
+                    .clipShape(
+                        RoundedCorner(radius : 100 , corners: [.topLeft, .bottomLeft])
+                    )
+            )
             .padding(.bottom , 80)
-            .padding(.leading , 100)
+            .padding(.leading , 50)
+            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(red: 58/255, green: 54/255, blue: 48/255))
     }
 }
 
+
+struct RoundedCorner:Shape{
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+    
+    
+}
 #Preview {
     ContentView()
 }
