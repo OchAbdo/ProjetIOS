58b2750094b8b0c1dc099a2d37f64543ed390ef1
Tekup-mac-7 <tekup-mac-7@Tekup-mac-7s-Mac-mini.local>
2025-04-26 10:00:12 +0100

    created splashScreen template

---
+++ ProjetIOS/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  ProjetIOS
+//
+//  Created by Tekup-mac-7 on 26/4/2025.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
